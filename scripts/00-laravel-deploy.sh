ea7cbccefe91c25532dfb819c31f073cc7c64209
Ava-Chang <ava849260123456@gmail.com>
2022-12-10 22:03:18 +0800

    modify deploy config

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo "Running composer"
 # composer update 
+composer require linecorp/line-bot-sdk
 composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
